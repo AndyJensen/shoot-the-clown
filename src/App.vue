bf8a541a92ad2761b406539ea6abe899f4723d36
Andy Jensen <andy@andy-jensen.com>
2022-02-18 17:02:43 -0600

    framed out target component

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,34 @@
     <PointBalls />
     <Scoreboard />
     <button v-on:click="updateScore(5)">Add Selected Ball Point Value</button>
+    <br /><br />
+    <div style="display: flex; flex-wrap: wrap;">
+      <Target status="active" color="red" />
+      <Target status="inactive" color="red" />
+      <Target status="active" color="yellow" />
+      <Target status="inactive" color="yellow" />
+      <Target status="active" color="blue" />
+      <Target status="inactive" color="blue" />
+      <Target status="active" color="nose" />
+      <Target status="inactive" color="nose" />
+      <Target status="active" color="eye" />
+      <Target status="inactive" color="eye" />
+    </div>
   </div>
 </template>
 
 <script>
 import PointBalls from "./components/PointBalls.vue"
 import Scoreboard from "./components/Scoreboard.vue"
-import { sharedData } from './components/Data.js'
-
+import Target from "./components/Target.vue"
+import { sharedData } from "./components/Data.js"
 
 export default {
   name: "App",
   components: {
     PointBalls,
-    Scoreboard
+    Scoreboard,
+    Target
   },
   data() {
     return {
---
+++ src/components/Target.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="ball"></div>
+</template>
+
+<script>
+export default {
+  name: "Target",
+  props: ["color", "status"],
+  mounted() {
+    this.$el.classList.add(`${this.color}_${this.status}`)
+  }
+}
+</script>
+
+<style scoped>
+.ball {
+  height: 130px;
+  width: 130px;
+  margin: 10px;
+}
+.red_active {
+  background: url("../assets/red_active.png") no-repeat center;
+}
+.red_inactive {
+  background: url("../assets/red_inactive.png") no-repeat center;
+}
+.yellow_active {
+  background: url("../assets/yellow_active.png") no-repeat center;
+}
+.yellow_inactive {
+  background: url("../assets/yellow_inactive.png") no-repeat center;
+}
+.blue_active {
+  background: url("../assets/blue_active.png") no-repeat center;
+}
+.blue_inactive {
+  background: url("../assets/blue_inactive.png") no-repeat center;
+}
+.nose_active {
+  background: url("../assets/red_nose_active.png") no-repeat center;
+}
+.nose_inactive {
+  background: url("../assets/red_nose_inactive.png") no-repeat center;
+}
+.eye_active {
+  background: url("../assets/blue_eye_active.png") no-repeat center;
+}
+.eye_inactive {
+  background: url("../assets/blue_eye_inactive.png") no-repeat center;
+}
+</style>
