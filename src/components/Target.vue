dc8452d23e6547760b2515a9edab46894b1a104d
Andy Jensen <andy@andy-jensen.com>
2022-06-19 01:51:05 -0500

    separate target size css variable

--- src/components/Target.vue
+++ src/components/Target.vue
@@ -17,47 +17,47 @@ export default {
 
 <style scoped>
 .ball {
-  height: var(--ball-size);
-  width: var(--ball-size);
+  height: var(--target-size);
+  width: var(--target-size);
 }
 .red_active {
   background: url("../assets/red_active.png") no-repeat center;
-  background-size: var(--ball-size);
+  background-size: var(--target-size);
 }
 .red_inactive {
   background: url("../assets/red_inactive.png") no-repeat center;
-  background-size: var(--ball-size);
+  background-size: var(--target-size);
 }
 .yellow_active {
   background: url("../assets/yellow_active.png") no-repeat center;
-  background-size: var(--ball-size);
+  background-size: var(--target-size);
 }
 .yellow_inactive {
   background: url("../assets/yellow_inactive.png") no-repeat center;
-  background-size: var(--ball-size);
+  background-size: var(--target-size);
 }
 .blue_active {
   background: url("../assets/blue_active.png") no-repeat center;
-  background-size: var(--ball-size);
+  background-size: var(--target-size);
 }
 .blue_inactive {
   background: url("../assets/blue_inactive.png") no-repeat center;
-  background-size: var(--ball-size);
+  background-size: var(--target-size);
 }
 .nose_active {
   background: url("../assets/red_nose_active.png") no-repeat center;
-  background-size: var(--ball-size);
+  background-size: var(--target-size);
 }
 .nose_inactive {
   background: url("../assets/red_nose_inactive.png") no-repeat center;
-  background-size: var(--ball-size);
+  background-size: var(--target-size);
 }
 .eye_active {
   background: url("../assets/blue_eye_active.png") no-repeat center;
-  background-size: var(--ball-size);
+  background-size: var(--target-size);
 }
 .eye_inactive {
   background: url("../assets/blue_eye_inactive.png") no-repeat center;
-  background-size: var(--ball-size);
+  background-size: var(--target-size);
 }
 </style>
